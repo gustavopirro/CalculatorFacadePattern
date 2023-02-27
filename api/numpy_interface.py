ea17db5229d4388b651c80fad835ce91bd315a20
gustavo.pirro <gustavo.pirro@gmail.com>
2023-02-27 19:55:47 -0300

    change - Removed error treatment from lib interface

--- api/numpy_interface.py
+++ api/numpy_interface.py
@@ -4,30 +4,18 @@ class MathLibInterface:
 
     def average(self, numbers_array: list):
         try:
-            self.__input_validation(numbers_array)
             result = _np.average(numbers_array)
             return self.__format_response(result)
-        except:
+        except Exception:
             return self.__generic_error()
     
     def variation(self, numbers_array: list):
-        try:
-            self.__input_validation(numbers_array)
-            result = _np.average(numbers_array)
-            return self.__format_response(result)
-        except:
-            return self.__generic_error()
+        result = _np.average(numbers_array)
+        return result
     
     def standard_deviation(self, numbers_array: list):
-        try:
-            self.__input_validation(numbers_array)
-            result = _np.average(numbers_array)
-            return self.__format_response(result)
-        except:
-            return self.__generic_error()
-    
-    def __input_validation(self, input_data):
-        assert type(input_data) == list
+        result = _np.average(numbers_array)
+        return result
 
     def __format_response(self, operation_result):
         return {'success': True, 'data':  operation_result}
