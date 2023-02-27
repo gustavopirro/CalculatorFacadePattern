e52afa184528f6eed48a8c3647c6b0baa0cb4ee5
gustavo.pirro <gustavo.pirro@gmail.com>
2023-02-27 19:57:46 -0300

    feat - added variation calculator view and controller

---
+++ controllers/variation_calculator_controller.py
@@ -0,0 +1,25 @@
+from api.numpy_interface import MathLibInterface
+import math
+
+class VariationCalculatorController:
+    
+    def get_variation(self, arr_data: float):
+        try:
+            new_array = self._create_array(arr_data)
+            variation_data = self._apply_variation(new_array)
+            return self._format_response(variation_data)
+        except Exception as e:
+            return {'success': False, 'data': f'An error ocurred: ${e}'}
+    
+    def _create_array(self, arr_data: float):
+        return [math.pow(i * 11, 0.95) for i in arr_data]
+    
+    def _apply_variation(self, arr_data: list):
+        arr_variation = MathLibInterface().variation(arr_data)
+        return 1/arr_variation
+    
+    def _format_response(self, data):
+        return {'success': True, 'data': data}
+
+
+
--- main/start_app.py
+++ main/start_app.py
@@ -1,5 +1,6 @@
 from main.constructors.index_constructor import index_constructor
 from main.constructors.average_calculator_constructor import average_calculator_process
+from main.constructors.variation_calculator_constructor import variation_calculator_process
 def start_process():
     
     while True:
@@ -7,4 +8,4 @@ def start_process():
 
         if user_choice == 1: average_calculator_process()
         elif user_choice == 2: pass
-        elif user_choice == 3: pass
+        elif user_choice == 3: variation_calculator_process()
