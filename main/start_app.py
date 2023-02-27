6eae6b729008b66e4301814a80dcf24e34deab45
gustavo.pirro <gustavo.pirro@gmail.com>
2023-02-27 20:33:37 -0300

    change - added comparison calculator in menu options

--- main/start_app.py
+++ main/start_app.py
@@ -1,11 +1,13 @@
 from main.constructors.index_constructor import index_constructor
 from main.constructors.average_calculator_constructor import average_calculator_process
 from main.constructors.variation_calculator_constructor import variation_calculator_process
+from main.constructors.comparison_calculator_constructor import comparison_calculator_process
+
 def start_process():
     
     while True:
         user_choice = index_constructor()
 
         if user_choice == 1: average_calculator_process()
-        elif user_choice == 2: pass
-        elif user_choice == 3: variation_calculator_process()
+        elif user_choice == 2: variation_calculator_process()
+        elif user_choice == 3: comparison_calculator_process()
--- views/index_view.py
+++ views/index_view.py
@@ -1,6 +1,6 @@
 def index_view():
     print('1 - Calcular média')
     print('2 - Calcular desvio padrão')
-    print('3 - Calcular variância')
+    print('3 - Comparar média e desvio padrão')
 
     return int(input('Insira a operação desejada: '))
