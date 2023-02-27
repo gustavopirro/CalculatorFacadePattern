acf9aec70436072931b1e2957e4d872d4bef7167
gustavo.pirro <gustavo.pirro@gmail.com>
2023-02-27 20:45:26 -0300

    fix - fixed comparison calculator calling wrong method from interface

--- controllers/comparison_calculator_controller.py
+++ controllers/comparison_calculator_controller.py
@@ -5,8 +5,9 @@ class ComparisonCalculatorController:
     
     def get_comparison(self, arr_data: float):
         variation_data = self._get_variation(arr_data)
-        average_data = self._get_average(arr_data)
-        result = self._data_comparison(variation_data, average_data)
+        standard_deviation_data = self._get_standard_deviation(arr_data)
+        result = self._data_comparison(variation_data, standard_deviation_data)
+        
         return self._format_response(result)
     
     def _create_array(self, arr_data: float):
@@ -16,17 +17,17 @@ class ComparisonCalculatorController:
         arr_variation = MathLibInterface().variation(arr_data)
         return arr_variation
     
-    def _get_average(self, arr_data: list):
-        arr_average = MathLibInterface().average(arr_data)
-        return arr_average
+    def _get_standard_deviation(self, arr_data: list):
+        arr_standard_deviation = MathLibInterface().standard_deviation(arr_data)
+        return arr_standard_deviation
     
-    def _data_comparison(self, variation_data, average_data):
-        return variation_data < average_data
+    def _data_comparison(self, variation_data, standard_deviation_data):
+        return variation_data < standard_deviation_data
 
-    def _format_response(self, variation_is_gt_average):
-        if variation_is_gt_average:
+    def _format_response(self, variation_is_gt_standard_deviation):
+        if variation_is_gt_standard_deviation:
             return {'success': True, 'data': "Variation is greater than average!"}
-        return {'success': False, 'data': "The comparison failed, variation must be greater than average!"}
+        return {'success': False, 'data': "The comparison failed, variation must be greater than standard deviation!"}
 
 
 
